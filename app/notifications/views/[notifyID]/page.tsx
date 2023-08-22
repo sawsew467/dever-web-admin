4f2b9c95e789282f76ed4fb48ceb3440bb2ff243
nhathuy <nguyenkhoanhathuy@gmail.com>
2023-08-22 15:40:25 +0700

    huy-notification

--- app/notifications/views/[notifyID]/page.tsx
+++ app/notifications/views/[notifyID]/page.tsx
@@ -1,23 +1,93 @@
-"use client"
-import React from 'react'
-import {useAppContext } from '@/app/context/AppContext'
+"use client";
+import React from "react";
+import Image from "next/image";
+import { useAppContext } from "@/app/context/AppContext";
+import pencil from "../../../../assets/images/notifications/pencil-alt.svg";
+import clock from "../../../../assets/images/notifications/clock.svg";
+import arrowleft from "../../../../assets/images/notifications/arrow-narrow-left.svg";
+import notification from "../../../../assets/images/notifications/notification.svg";
+
+import Link from "next/link";
 
 interface pageProps {
-    params: {notifyID: string};
+  params: { notifyID: string };
 }
 
-function DetailNotification({params}: pageProps) {
-    const {isOpenSlidebar, isMouseVisit} = useAppContext();
+function DetailNotification({ params }: pageProps) {
+  const { isOpenSlidebar, isMouseVisit } = useAppContext();
+  const data = {
+    title: "Noteworthy technology acquisitions 2021",
+    time: "August 7th 2023, 6:25:59 am",
+    img: notification,
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tincidunt ultrices tempus. Sed quis arcu nec nibh faucibus feugiat non et nunc. Proin bibendum turpis consequat tellus dapibus, eget commodo ligula mattis. Nullam finibus, odio at tincidunt tincidunt, ante nisl maximus justo, in lobortis metus felis faucibus est. Mauris ultrices tincidunt vehicula. Suspendisse scelerisque urna sapien, ac varius ex luctus quis. Nullam luctus sollicitudin tortor vitae auctor. In non arcu metus. Nam venenatis dignissim felis, non pharetra urna blandit eget. Fusce pulvinar magna in nunc dignissim molestie. Suspendisse sollicitudin nulla orci, vel hendrerit dui consequat nec. Aenean magna odio, tempus ut tempor vel, volutpat a augue. Suspendisse potenti. Integer imperdiet aliquet sapien et auctor.",
+  };
 
-    return (
-        <div className={`w-[100%] ${isOpenSlidebar ? isMouseVisit ? "sm:w-[calc(100%-250px)]" : "sm:w-[calc(100%-65px)]" : "sm:w-[calc(100%-250px)]"} absolute right-0 top-[72px] bottom-0 h-fit duration-[0.3s]`}>
-            <div>
-                <h1 className='text-center mt-[100px] text-[30px] font-bold text-black dark:text-gray-500'>
-                    This is: {params.notifyID} detail notification page</h1>
-                {/* Write page content here */}
-            </div>
+  return (
+    <div
+      className={`w-[100%] ${
+        isOpenSlidebar
+          ? isMouseVisit
+            ? "sm:w-[calc(100%-250px)]"
+            : "sm:w-[calc(100%-65px)]"
+          : "sm:w-[calc(100%-250px)]"
+      } absolute right-0 top-[72px] bottom-0 h-fit duration-[0.3s] px-[16px] pb-[50px]`}
+    >
+      <div className="">
+        {/* This is: {params.notifyID} detail notification page</h1> */}
+        <div className="flex justify-between mt-[20px] mb-[30px]">
+          <p className="text-[24px] font-bold ">Notifications Details</p>
+          <div className="flex">
+            <button className="bg-[#046C4E] h-[34px] w-[155px] mt-[1.5px] flex rounded-lg text-center px-[12px]">
+              <Image
+                src={pencil}
+                alt="Picture of the author"
+                className="cursor-pointer w-[16px] h-[16px] my-[8px] mr-[8px] "
+              />
+              <Link
+                href={"/notifications/create"}
+                className="text-[#fff] text-center text-[12px] font-medium pt-[8px] h-[18px]"
+              >
+                Create notification
+              </Link>
+            </button>
+            <button className="bg-[#1A56DB] h-[34px] ml-[20px] mt-[1.5px] flex rounded-lg text-center px-[12px]">
+              <Image
+                src={arrowleft}
+                alt="Picture of the author"
+                className="cursor-pointer w-[16px] h-[16px] my-[8px] mr-[8px] "
+              />
+              <Link
+                href={"/notifications/create"}
+                className="text-[#fff] text-center text-[12px] font-medium pt-[8px] h-[18px]"
+              >
+                Notifications
+              </Link>
+            </button>
+
+          </div>
         </div>
-    )
+        <div className="px-[30px] border-[1px] pt-[31px] pb-[33px] rounded-[10px]">
+          <p className="text-[36px] font-bold ">{data.title}</p>
+          <div className="flex pt-[30px]">
+            <Image
+              src={clock}
+              alt="Picture of the author"
+              className="cursor-pointer mr-[8px]"
+            />
+            <p className="text-[#6B7280]">{data.time}</p>
+          </div>
+          <Image
+            src={data.img}
+            alt="Picture of the author"
+            className="cursor-pointer w-full mt-[30px]"
+          />
+          <p className="pt-[30px] font-medium text-[20px]">{data.content}</p>
+          <Link href={"/notifications/create"}></Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default DetailNotification
+export default DetailNotification;
--- app/notifications/views/page.tsx
+++ app/notifications/views/page.tsx
@@ -1,24 +1,167 @@
-"use client"
-import React from 'react'
-import {useAppContext } from '@/app/context/AppContext'
-import Link from 'next/link';
+"use client";
+import React from "react";
+import { useAppContext } from "@/app/context/AppContext";
+import Link from "next/link";
+import Image from "next/image";
 
-function View() {
-    const {isOpenSlidebar, isMouseVisit} = useAppContext();
+import notification from "../../../assets/images/notifications/notification.svg";
+import search from "../../../assets/images/notifications/search-outline.svg";
+import pencil from "../../../assets/images/notifications/pencil-alt.svg";
+import clock from "../../../assets/images/notifications/clock.svg";
+import arrow from "../../../assets/images/notifications/arrow-right.svg";
 
-    return (
-        <div className={`w-[100%] ${isOpenSlidebar ? isMouseVisit ? "sm:w-[calc(100%-250px)]" : "sm:w-[calc(100%-65px)]" : "sm:w-[calc(100%-250px)]"} absolute right-0 top-[72px] bottom-0 h-fit duration-[0.3s]`}>
-            <div>
-                <h1 className='text-center mt-[100px] text-[30px] font-bold text-black dark:text-gray-500'>This is view notification page</h1>
-                {/* Write page content here */}
-                <div className='text-center flex flex-col '>
-                    <Link href={"/notifications/views/1"} >View Detail Notification 1</Link>
-                    <Link href={"/notifications/views/2"} >View Detail Notification 2</Link>
-                    <Link href={"/notifications/views/3"} >View Detail Notification 3</Link>
-                </div>
-            </div>
+interface pageProps {
+  params: { viewID: string };
+}
+function View({ params }: pageProps) {
+  const data = [
+    {
+      img: notification,
+      title: "Noteworthy technology acquisitions 2021",
+      second_title:
+        "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+      time: "August 7th 2023, 6:25:59 am",
+    },
+    {
+      img: notification,
+      title: "Noteworthy technology acquisitions 2021",
+      second_title:
+        "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+      time: "August 7th 2023, 6:25:59 am",
+    },
+    {
+      img: notification,
+      title: "Noteworthy technology acquisitions 2021",
+      second_title:
+        "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+      time: "August 7th 2023, 6:25:59 am",
+    },
+    {
+      img: notification,
+      title: "Noteworthy technology acquisitions 2021",
+      second_title:
+        "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+      time: "August 7th 2023, 6:25:59 am",
+    },
+    {
+      img: notification,
+      title: "Noteworthy technology acquisitions 2021",
+      second_title:
+        "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+      time: "August 7th 2023, 6:25:59 am",
+    },
+    {
+      img: notification,
+      title: "Noteworthy technology acquisitions 2021",
+      second_title:
+        "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+      time: "August 7th 2023, 6:25:59 am",
+    },
+    {
+      img: notification,
+      title: "Noteworthy technology acquisitions 2021",
+      second_title:
+        "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+      time: "August 7th 2023, 6:25:59 am",
+    },
+  ];
+  const { isOpenSlidebar, isMouseVisit } = useAppContext();
+
+  return (
+    <div
+      className={`w-[100%] text-black ${
+        isOpenSlidebar
+          ? isMouseVisit
+            ? "sm:w-[calc(100%-250px)]"
+            : "sm:w-[calc(100%-65px)]"
+          : "sm:w-[calc(100%-250px)]"
+      } absolute right-0 top-[72px] bottom-0 h-fit duration-[0.3s] px-[17px]`}
+    >
+      <div>
+        <p className="text-[24px] font-bold mt-[20px]">All notifications</p>
+        <div className="h-[37px] my-[20px] flex w-full justify-between">
+          <div className="flex h-[37px] border-[1px] w-[518px] rounded-lg">
+            <select className="p-0 font-medium rounded-lg pl-[20px] border-none text-center  w-[84px] ">
+              <option value="All" className="text-black ">
+                All
+              </option>
+            </select>
+            <input
+              type="text"
+              placeholder="Search notifications..."
+              className="border-0 border-l-[1px] border-solid border-[#D1D5DB] w-full"
+            ></input>
+            <Image
+              src={search}
+              alt="Picture of the author"
+              className="cursor-pointer bg-[#1A56DB] w-[42px] px-[11px] rounded-r-lg "
+            />
+          </div>
+          <div className="bg-[#046C4E] h-[34px] w-[155px] mt-[1.5px] flex rounded-lg text-center px-[12px]">
+            <Image
+              src={pencil}
+              alt="Picture of the author"
+              className="cursor-pointer w-[16px] h-[16px] my-[8px] mr-[8px] "
+            />
+            <Link
+              href={"/notifications/create"}
+              className="text-[#fff] text-center text-[12px] font-medium pt-[8px] h-[18px]"
+            >
+              Create notification
+            </Link>
+          </div>
         </div>
-    )
+        <div
+          className={` grid grid-cols-2 gap-x-[20px] gap-y-[20px] text-black `}
+        >
+          {data.map((item) => (
+            <div>
+              <Image
+                src={item.img}
+                alt="Picture of the author"
+                className="cursor-pointer w-full"
+              />
+              <div className="p-[20px] rounded-b-2xl border-[1px] border-[#D1D5DB]">
+                <p className="text-[24px] font-bold">{item.title}</p>
+                <p className="text-[16px] text-gray-500 py-[8px]">
+                  {item.second_title}
+                </p>
+                <div className="flex">
+                  <Image
+                    src={clock}
+                    alt="Picture of the author"
+                    className="cursor-pointer mr-[10px]"
+                  />
+                  <p className="text-gray-500 text-[16px]">{item.time}</p>
+                </div>
+                <Link href={`/notifications/views/${item.title}`} >
+
+                <button className="rounded-lg mt-[16px] py-[10.5px] w-fit px-[20px] flex text-[#fff] bg-[#1A56DB]">
+                  <p>Read more</p>
+                  <Image
+                    src={arrow}
+                    alt="Picture of the author"
+                    className="cursor-pointer ml-[8px] "
+                    />
+                </button>
+                    </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="flex justify-center mt-[20px] mb-[20px]">
+        <div className="  text-[14px] rounded-[4px]  border-[1px]  ">
+
+          <button className="py-[6px] px-[12px]  ">Previous</button>
+          <button className="py-[6px] px-[12px] border-l-[1px]">1</button>
+          <button className="py-[6px] px-[12px] border-l-[1px]">2</button>
+          <button className="py-[6px] px-[12px] border-l-[1px]">3</button>
+          <button className="py-[6px] px-[12px] border-l-[1px]">Next</button>
+        </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default View
+export default View;
