5e3c97c90d078859f421f7cf5c5e87d481306db9
VuVuDev <vuvo070403@gmail.com>
2023-09-07 19:11:10 +0700

    vu

---
+++ components/SettingElement/ChangePassword.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ChangePassword() {
+    return (
+        <div className='flex flex-col gap-[20px] p-[24px] shadow-primary rounded-[10px]'>
+            
+        </div>
+    )
+}
+
+export default ChangePassword
