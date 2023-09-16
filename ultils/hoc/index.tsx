3976ed929bbad66e6696ea0ca9ec2480cc29d0e2
hoangvh238 <hoangvh238.dev@gmail.com>
2023-09-16 18:23:28 +0700

    fix router

--- ultils/hoc/index.tsx
+++ ultils/hoc/index.tsx
@@ -5,21 +5,12 @@ import { toast } from 'react-toastify';
 export default function withAuth(Component: React.ComponentType<any>) {
   return function ProtectedRoute(props: any) {
     const router = useRouter();
-
-    const checkAuthentication = async () => {
-      try {
-        const isAuthenticated = await getCookie('refreshToken');
+    const isAuthenticated =  getCookie('refreshToken');
         
-        if (isAuthenticated === null || isAuthenticated === undefined) {
-          toast.error('please login !!');
-          router.push('/auth/sign-in');
-        }
-        else  return <Component {...props} />;
-      } catch (error) {
-        console.error('Error while checking authentication:', error);
-      }
-    };
-
-    checkAuthentication();
+    if (isAuthenticated === null || isAuthenticated === undefined) {
+      toast.error('please login !!');
+      router.push('/auth/sign-in');
+    }
+    return <Component {...props} />;
   };
 }
